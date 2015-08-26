1a51375d9196d90cacca6afb486e38422954c6d1
Alex Baylon <aunesto15@gmail.com>
2015-08-26 10:37:32 -0500

    Initial Commit

---
+++ Threads_practica/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Threads_practica
+//
+//  Created by Alexander Arturo Baylon Ibanez on 26/08/15.
+//  Copyright (c) 2015 Alexander Arturo Baylon Ibanez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
